60fe123761746d48598327f114720896241c7415
paweljurczynski <pawel.jurczynski@live.com>
2018-10-13 12:07:22 +0200

    Implementation 2.2 [2.2]

---
+++ 2.2.js
@@ -0,0 +1,42 @@
+const fetch = require('isomorphic-fetch');
+
+run();
+
+async function run() {
+    const elementWithTransactionsRes = await fetch('http://localhost:8545/', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            jsonrpc: '2.0',
+            id: 1,
+            method: 'eth_getBlockByHash',
+            params: ['0x9cb93389710a04070b86dbfd8a1e7dbe6e14a3ed2c6a2b7468d8a9d00cc94b5f', false]
+        })
+    });
+
+    const elementWithTransactions = await elementWithTransactionsRes.json();
+    const transactionsHash = elementWithTransactions.result.transactions;
+
+    const res = await fetch('http://localhost:8545/', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            jsonrpc: '2.0',
+            id: 1,
+            method: 'eth_getTransactionByHash',
+            params: transactionsHash
+        })
+    });
+
+    const json = await res.json();
+    const { gas, gasPrice } = json.result;
+
+    console.log(`
+        Gas: ${gas}
+        Gas price: ${gasPrice}
+    `)
+}
